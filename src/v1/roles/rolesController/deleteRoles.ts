45a84e1e70a4469b16d0ec2a794aec843015f0bb
gufron <gufronnewbiners@gmail.com>
2024-11-22 07:51:46 +0700

    DELETE TEST

--- src/v1/roles/rolesController/deleteRoles.ts
+++ src/v1/roles/rolesController/deleteRoles.ts
@@ -4,7 +4,6 @@ export const DeleteRoles = async (req : Request, res : Response): Promise<void> 
     try{
         const id = req.params.id
         const deleteRoles = await roles.findByIdAndDelete({_id : id});
-        res.status(200).json('TEST');
         const getRoles = await roles.find();
         res.status(200).json(getRoles);
     }catch (err) {
