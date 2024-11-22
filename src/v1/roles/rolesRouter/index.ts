1cd78e2d58a9436fb8580b0f62d51831240b9549
gufron <gufronnewbiners@gmail.com>
2024-11-22 07:33:30 +0700

    menambahkan-roles

--- src/v1/roles/rolesRouter/index.ts
+++ src/v1/roles/rolesRouter/index.ts
@@ -4,9 +4,7 @@ import { PostRoles } from '../rolesController/postRoles';
 import { GetAllRoles } from '../rolesController/getAllRoles';
 import { DeleteRoles } from '../rolesController/deleteRoles';
 router.post('/', PostRoles);
-
 router.get('/', GetAllRoles);
-
 router.get('/:id', (req, res) => {
   res.send(`Hello, world! test get id ${req.params.id}`);
 });
