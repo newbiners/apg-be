bcfcdfca5790a558cef8244a03a4b713897e0b3e
gufron <gufronnewbiners@gmail.com>
2024-11-25 16:18:01 +0700

    test

--- src/v1/users/usersController/signup.ts
+++ src/v1/users/usersController/signup.ts
@@ -1,27 +1,28 @@
 import { User } from "../usersModel/userModel";
 import { Request, Response } from 'express'
 import bcrypt from 'bcryptjs'
-export const signup = async (req : Request, res : Response): Promise<void> => {
-    try{
-        const { name, username, password, confirmPassword, role} = req.body;
+export const signup = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { name, username, password, confirmPassword, role } = req.body;
 
-        if(password != confirmPassword){
-            res.status(400).json({message : "Password not match"})
+        res.json({ name, username, password, confirmPassword, role });
+        if (password != confirmPassword) {
+            res.status(400).json({ message: "Password not match" })
         }
         const salt_id = process.env.SALT;
-        const salt =  bcrypt.genSaltSync(Number(salt_id));
+        const salt = bcrypt.genSaltSync(Number(salt_id));
         const hashPassword = bcrypt.hashSync(password, salt);
 
         const newUser = new User({
             name,
             username,
-            password : hashPassword,
+            password: hashPassword,
             role
         });
         const saveUser = await newUser.save();
         res.json(saveUser);
-    }catch(error){ 
-        res.status(400).json({message : error})
+    } catch (error) {
+        res.status(400).json({ message: error })
     }
 }
 
