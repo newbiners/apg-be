c3391d49064ca6e116a7fe120150edfaa1fc9d3d
gufron <gufronnewbiners@gmail.com>
2024-11-27 14:23:24 +0700

    test

--- src/v1/nilaiJuri/nilaiJuriController/getNilaiJuri.ts
+++ src/v1/nilaiJuri/nilaiJuriController/getNilaiJuri.ts
@@ -17,7 +17,7 @@ export const getNilaiJuri = async (
         const token = create && create.split(' ')[1];
         const decoded: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string);
 
-        // res.status(200).json(decoded);
+        res.status(200).json(decoded.payload);
 
 
         const getData = await nilaiJuri.find({
