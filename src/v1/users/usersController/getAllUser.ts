ae814a6dfe8c1616eb8520be0949c7594eaff465
gufron <gufronnewbiners@gmail.com>
2024-12-02 08:29:24 +0700

    test

--- src/v1/users/usersController/getAllUser.ts
+++ src/v1/users/usersController/getAllUser.ts
@@ -66,7 +66,7 @@ export const GetAllUsers = async (req: Request, res: Response): Promise<void> =>
         const usersWithRoles = getUsers.map((user) => {
             const userWithRole = user.toObject(); // Convert Mongoose document to plain object
             userWithRole.role = getRoles[user.role.toString()] || null; // Assign role or null if not found
-            userWithRole.lomba = user.lomba ? lombaReturn[user.lomba.toString()] || null : null; 
+            // userWithRole.lomba = user.lomba ? lombaReturn[user.lomba.toString()] || null : null; 
             return userWithRole;
         });
 
