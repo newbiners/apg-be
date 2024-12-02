655519733831ce5c98e01e95807ebb7c335ac6cf
gufron <gufronnewbiners@gmail.com>
2024-12-02 08:56:51 +0700

    test

--- src/v1/lomba/lombaController/getAllLomba.ts
+++ src/v1/lomba/lombaController/getAllLomba.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express";
 import { lomba } from "../lombaModel/lombaModel";
+import { User } from "../../users/usersModel/userModel";
+import jwt from 'jsonwebtoken'
 export const GetAllLomba = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    const getLomba = await lomba.find();
+    const { user } = req.body
+    var filter: any = {};
+
+    if (user) {
+      const token = user && user.split(' ')[1];
+      const decoded: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string);
+      const currentUser: any = await User.findOne({ _id: decoded.payload });
+
+      if (currentUser.lomba) {
+        filter._id = currentUser.lomba
+      }
+    }
+    const getLomba = await lomba.find(filter);
 
     res.status(200).json(getLomba);
   } catch (err) {
