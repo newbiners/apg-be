7c1b47fdd30d469ee90672fd962e04b39fcfbfc1
gufron <gufronnewbiners@gmail.com>
2024-12-11 14:28:53 +0700

    test

--- src/v1/generate_juara_umum/juaraUmumController/postJuaraUmumRegu.ts
+++ src/v1/generate_juara_umum/juaraUmumController/postJuaraUmumRegu.ts
@@ -188,7 +188,7 @@ export const postJuaraUmumRegu = async (
         //   item.nilai_juara = index === 0 ? 5 : index === 1 ? 3 : index === 2 ? 1 : 0;
         // });
 
-        const lombaName = lombaItem._id.toString();
+        const lombaName = lombaItem.name.toString();
 
         if (!dataArr[lombaName]) {
           dataArr[lombaName] = [];
