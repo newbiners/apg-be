516ce12f7bd43f5160fa9c9caf265c31d5c1bd26
gufron <gufronnewbiners@gmail.com>
2025-01-09 06:03:14 +0700

    test

--- src/v1/lomba/lombaRouter/index.ts
+++ src/v1/lomba/lombaRouter/index.ts
@@ -9,12 +9,12 @@ import { Lomba } from "../lombaController/lomba";
 import { exportExl } from "../lombaController/exportExl";
 const router = express.Router();
 
+router.post("/export", (req, res) => res.status(200).json({ message: "success" }));
 router.post("/user", Lomba);
 router.use(authenticateToken, authorizeRoles(['ADMIN', 'JURI']))
 router.get("/", GetAllLomba);
 router.post("/:id", GetAllLomba);
 router.use(authenticateToken, authorizeRoles(['ADMIN']))
-router.post("/export", exportExl);
 router.post("/", PostLomba);
 router.delete("/:id", DeleteLomba);
 router.put("/:id", EditLomba);
